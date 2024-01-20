dc4e7ee79c2295ee1e556cbe33f5aa7e053517bf
José Reynaldo <ja.reynaldo@outlook.com>
2024-01-19 19:16:38 -0500

    unfinished v1

--- Labs/01 Scientific calculator/calculator.py
+++ Labs/01 Scientific calculator/calculator.py
@@ -1,16 +1,108 @@
-#
+# Authors: Jose Reynaldo and Olusola Awonusonu
+# Date: January 19th, 2023
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# will print the menu
+def printMenu():
+    print("Calculator Menu")
+    print("---------------")
+    print("0. Exit Program")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exponentiation")
+    print("6. Logarithm")
+    print("7. Display Average \n")
+
+
+
+def addition(one, two):
+    return one + two
+
+def subtraction(one, two):
+    return one - two
+
+def multiplication(one, two):
+    return one *two
+
+def division(one, two):
+    return one / two
+
+def exponentiation(one, two):
+    return one ** two
+
+def logarithm(base, x):
+    return math.log(x, base)
+
+
+
+# prompts users for choice from the menu
+def askForMenuSelection():
+    print("Enter Menu Selection: ", end="")
+    userchoice = input()
+
+    return int(userchoice)
+
+# prompts users for what two numbers the opperations
+# should be done with
+def askForNumbers():
+    print("Enter first operand: ", end="")
+    firstNumber = float(input())
+    print("Enter second operand: ", end="")
+    secondNumber = float(input())
+    print()
+
+    return firstNumber, secondNumber
+
+# determines which value was chosen from the menu
+def whichValue(userChoice, firstValue, secondValue):
+    if(userChoice == 1):
+        return addition(firstValue, secondValue)
+
+    elif(userChoice == 2):
+        return subtraction(firstValue, secondValue)
+
+    elif(userChoice == 3):
+        return multiplication(fisrstValue, secondValue)
+
+    elif(userChoice == 4):
+        return division(firstValue, secondValue)
+
+    elif(userChoice == 5):
+        return exponentiation(firstValue, secondValue)
+
+    else:
+        return logarithm(firstValue, secondValue)
+
+
+
+
+
+
+def main():
+    userChoice = 1
+    checker = True
+
+    total = 0
+    counter = 0
+    while checker:
+        printMenu()
+        userChoice = askForMenuSelection()
+        if userChoice == 0:
+            quit()
+        choiceOne, ChoiceTwo = askForNumbers()
+
+        if(userChoice != 7):
+            whichValue()
+
+
+        print(choiceOne, ChoiceTwo)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
