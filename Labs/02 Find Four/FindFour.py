6bd15eacd626b8c7bd227413fdd1b5d88f7295f8
José Reynaldo <ja.reynaldo@outlook.com>
2024-01-25 13:36:13 -0500

    first bit complete

--- Labs/02 Find Four/FindFour.py
+++ Labs/02 Find Four/FindFour.py
@@ -1,7 +1,7 @@
 # Author: Jose Reynaldo
 # Date: 25/1/23
 
-# will check if it's a string here bc i could not get it to work otherwise
+# determine height and will check if it's a string here bc i could not get it to work otherwise
 def askForHeight():
     print("Enter height of board (rows): ", end="")
     height = input()
@@ -13,8 +13,7 @@ def askForHeight():
             height = askForHeight()
     return height
 
-
-
+#same thing as askForHeight except for width
 def askForWidth():
     print("Enter width of board (columns): ", end="")
     width = input()
@@ -27,9 +26,10 @@ def askForWidth():
     return width
 
 
+
+# manages the asking for dimensions, and berefies it's within the right bounds
 def askForDimensions():
     height = int(askForHeight())
-
 
     while height < 4:
         print("Error: height must be at least 4!")
@@ -61,18 +61,74 @@ def get_initial_board(rows, columns):
 
     return board
 
+def print_board(board, height, width):
+
+    for i in range(width + 2):
+        #to determine the space at start and end
+        if i == 0 or i == width + 1:
+            print(" ", end="")
+        else:
+            print("__", end="")
+
+    print()
+    for i in range(height):
+        print("|", end="")
+        for j in range(width):
+            print(board[i][j] + " ", end="")
+        print("|")
+
+    for k in range(width + 2):
+        if k == 0 or k == width + 1:
+            print(" ", end="")
+        else:
+            print("--", end="")
+
+def isColInBound(choice, column):
+
+    while choice < column or choice > column:
+        print("Error: no such column!")
+        height = playerOneSelection(column)
+
+
+
+def playerOneSelection(column):
+
+    print("Player 1 - Select a Column: ", end="")
+    playerChoice = input()
+
+    if playerChoice.isdigit():
+        choice = int(playerChoice)
+        returnVal = isColInBound(choice, column)
+        return returnVal
+
+    else:
+        while isinstance(playerChoice, str):
+            print("Error: not a number!")
+            playerChoice = playerOneSelection(column)
+    return playerChoice
+
+
 
 def main():
     checker = True
+
+    print("Welcome to Find Four!")
+    print("---------------------")
+
+    height, width = askForDimensions()
+
+    board = get_initial_board(height, width)
+    print_board(board, height, width)
+
+    print()
+    print("Player 1: x")
+    print("Player 2: o")
+    print()
+
     while checker:
-        print("Welcome to Find Four!")
-        print("---------------------")
+        playerOneSelection(width)
 
-        height, width = askForDimensions()
 
-        board = get_initial_board(height, width)
-
-        print(board)
         checker = False
 
 
