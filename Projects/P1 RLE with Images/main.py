6a0b301e7c8cd6a9a963c8e262078b9b8fd5072d
José Reynaldo <ja.reynaldo@outlook.com>
2024-02-10 13:03:27 -0500

    hardcoded but encode_rle works

--- Projects/P1 RLE with Images/main.py
+++ Projects/P1 RLE with Images/main.py
@@ -49,12 +49,50 @@ def to_hex_string(data_in):
                 data_out.append(str(values[i]))
     return "".join(data_out)
 
+def to_hex_string_single_val(data_in):
+    data_out = ""
+    values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g"]
+
+    for i in range(16):
+        if data_in == i:
+            data_out = str(values[i])
+    return data_out
+
+
+
 def encode_rle(data_in):
     data_out = ["b'"]
-
     runs = count_runs(data_in)
-    for counter in runs:
 
+
+    how_many_instances = 0
+    last_char = data_in[0]
+    counter = 0
+    # for however many indivdual character sequences you have (runs)
+    # the program will loop through them, adding one to the how_many_instances
+    # var until they no longer equal each other. Then the instances will be uploaded
+    # to the data_out list and the actual value will be converted to hex
+    for i in range(runs):
+
+        while data_in[counter] == last_char:
+
+            how_many_instances += 1
+            counter += 1
+            last_char = data_in[counter - 1]
+            if counter == len(data_in):
+                counter -= 1
+                last_char = None
+
+        append_string = r"\x0" + to_hex_string_single_val(how_many_instances)
+        data_out.append( append_string)
+
+        append_string = r"\x0" + to_hex_string_single_val(data_in[counter - 1])
+        data_out.append(append_string)
+        how_many_instances = 0
+        last_char = data_in[counter]
+
+
+    return "".join(data_out)
 
 
 
@@ -69,8 +107,9 @@ def main():
 
     current_data = []
 
-
+    print(encode_rle([15,15,15,4,4,4,8,8,8,8,4,4,4]))
     print()
+
     checker = True
     while checker:
         print()
@@ -90,7 +129,7 @@ def main():
             current_data = console_gfx.load_file(file_name)
 
         elif int(user_choice) == 2:
-            checker = console_gfx.TEST_IMAGE
+            image_data = console_gfx.TEST_IMAGE
             print("Test image data loaded.")
 
         elif int(user_choice) == 3:
